14e162c05178d682f43e08e54e347fd0120c4fbf
EC2 Default User <ec2-user@ip-172-31-45-189.ec2.internal>
2020-11-05 13:52:26 +0000

    devise leader追加

--- hobby_pot/config/routes.rb
+++ hobby_pot/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :leaders, controllers: {
+    sessions: 'leaders/sessions'
+  }
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
