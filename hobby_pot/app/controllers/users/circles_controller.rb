e14f2521bf2b9a83829867d06e088f0349863753
EC2 Default User <ec2-user@ip-172-31-45-189.ec2.internal>
2020-11-28 06:46:31 +0000

    レイアウト調整

--- hobby_pot/app/controllers/users/circles_controller.rb
+++ hobby_pot/app/controllers/users/circles_controller.rb
@@ -6,11 +6,11 @@ class Users::CirclesController < ApplicationController
 
   def index
     if params[:category_id]
-      @category_list = Category.all
+      @category_list = Category.all.sample(10)
       @list = Category.find(params[:category_id])
       @circles = @list.circles.where(circle_status: true).or(@list.circles.where('start_time >= ?', Date.today)).page(params[:page]).per(15).reverse_order
     else
-      @category_list = Category.all
+      @category_list = Category.all.sample(10)
       @circles = Circle.where(circle_status: true).or(Circle.where('circles.start_time >= ?', DateTime.now)).page(params[:page]).per(15).reverse_order
     end
   end
