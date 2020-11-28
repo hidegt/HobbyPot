ef2d053d3d02f28f8f00735bf55f33f424c152c5
EC2 Default User <ec2-user@ip-172-31-45-189.ec2.internal>
2020-11-28 09:28:47 +0000

    調整ラスト

--- hobby_pot/app/controllers/users/circles_controller.rb
+++ hobby_pot/app/controllers/users/circles_controller.rb
@@ -1,17 +1,17 @@
 class Users::CirclesController < ApplicationController
   # require 'date'
   def top
-    @circles = Circle.where(circle_status: true).or(Circle.where(:start_time => Date.today)).sample(4)
+    @circles = Circle.where(circle_status: true).where('start_time >= ?', Time.current.beginning_of_day).sample(4)
   end
 
   def index
     if params[:category_id]
       @category_list = Category.all.sample(10)
       @list = Category.find(params[:category_id])
-      @circles = @list.circles.where(circle_status: true).or(@list.circles.where('start_time >= ?', Date.today)).page(params[:page]).per(15).reverse_order
+      @circles = @list.circles.where(circle_status: true).where('start_time >= ?', Time.current.beginning_of_day).page(params[:page]).per(15).reverse_order
     else
       @category_list = Category.all.sample(10)
-      @circles = Circle.where(circle_status: true).or(Circle.where('circles.start_time >= ?', DateTime.now)).page(params[:page]).per(15).reverse_order
+      @circles = Circle.where(circle_status: true).where('start_time >= ?', Time.current.beginning_of_day).page(params[:page]).per(15).reverse_order
     end
   end
 
@@ -21,7 +21,7 @@ class Users::CirclesController < ApplicationController
     @join_circle = JoinCircle.new
     @circle_categories = @circle.categories
   end
-  
+
   def join_circles
     @circle = Circle.find(params[:id])
   end
