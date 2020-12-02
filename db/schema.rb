ba21acc76da34233be19c466e6bfaafe4b98b35a
EC2 Default User <ec2-user@ip-172-31-45-189.ec2.internal>
2020-12-02 04:57:53 +0000

    訂正

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_12_02_040549) do
     t.integer "circle_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    # t.decimal "score_", precision: 5, scale: 3
     t.decimal "score_comment", precision: 5, scale: 3
     t.index ["circle_id"], name: "index_circle_comments_on_circle_id"
     t.index ["user_id"], name: "index_circle_comments_on_user_id"
